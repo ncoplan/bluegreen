7de480e3ff101b59a321ed1f66c88d19e80f84d4
ncoplan <neil@coplan.net>
2018-05-01 14:31:49 -0400

    Update image.php

--- image.php
+++ image.php
@@ -4,7 +4,7 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
 # $deployment = getenv("COLOR");
-$deployment = getenv("GREEN");
+$deployment = 'green';
 
 
 if ( $deployment == 'blue') {
